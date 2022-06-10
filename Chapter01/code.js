4dae711ad842f97310194305d8ae681bbfa44427
abeljumanuel <abeljumanuel.rojas@gmail.com>
2022-06-10 17:03:37 -0500

    Variables

--- Chapter01/code.js
+++ Chapter01/code.js
@@ -2,5 +2,9 @@
 //string = "cadena de texto"
 //number = 19
 //booleano = false
-let number;
-alert(number)
+const numberconst = "Juan";
+let numbernull = null;
+let number1 = 35, number2 = 36, number3 = "Juan";
+alert(numberconst+" - "+number1+" - "+number2*number3)
+number1 = prompt("Write a verb in third person:")
+alert("My name is "+number3+" i'm "+number2+" years old and "+number1)
